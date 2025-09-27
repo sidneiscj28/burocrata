bcb211b1591935f2f895dc5df8972e6b02b9d38c
sidneicjr28 <sidneicorreiajunior2013@gmail.com>
2025-09-27 17:40:46 -0300

    included sugested project to modify

---
+++ src/estudantes/entidades/Burocrata.java
@@ -0,0 +1,64 @@
+package estudantes.entidades;
+
+import professor.entidades.*;
+
+/**
+ * Classe que traz a lógica do algoritmo de organização e despacho de processos.
+ * <br><br>
+ * Você pode incluir novos atributos e métodos nessa classe para criar
+ * lógicas mais complexas para o gerenciamento da organização e despacho de 
+ * processos, mas esses <strong>atributos e métodos devem ser todos 
+ * privados</strong> e eles não serão invocados diretamente pelo simulador.
+ * <br><br>
+ * Os únicos métodos públicos devem ser: getEstresse, trabalhar, estressar e
+ * estressarMuito.
+ * 
+ * @author coloque os nomes dos autores aqui
+ */
+public class Burocrata {
+    private int estresse = 0;
+    private Mesa mesa;
+    private Universidade universidade;
+    
+    /**
+     * Construtor de Burocrata.
+     * 
+     * @param m mesa com os processos
+     * @param u universidade com os montes dos cursos e a secretaria
+     */
+    public Burocrata(Mesa m, Universidade u){
+        this.mesa = m;
+        this.universidade = u;
+    }
+    
+    /**
+     * Executa a lógica de criação e despacho dos processos.
+     * <br><br>
+     * Esse método é o único método de controle invocado durante a simulação 
+     * da universidade.
+     * <br><br>
+     * Aqui podem ser feitas todas as verificações sobre os documentos nos 
+     * montes dos cursos e dos processos abertos na mesa do Burocrata. A partir 
+     * dessas informações, você pode colocar documentos nos processos abertos
+     * e despachar os processos para a secretaria acadêmica.
+     * <br><br>
+     * Cuidado com a complexidade do seu algoritmo, porque se ele demorar muito
+     * serão criados menos documentos na sua execução e sua produtividade geral
+     * vai cair.
+     * <br><br>
+     * Esse método será chamado a cada 100 milissegundos pelo simulador da
+     * universidade.
+     * <br><br>
+     * <strong>O burocrata não pode manter documentos com ele</strong> depois
+     * que o método trabalhar terminar de executar, ou seja, você deve devolver
+     * para os montes dos cursos todos os documentos que você removeu dos montes
+     * dos cursos.
+     * 
+     * @see professor.entidades.Universidade#despachar(Processo)
+     * @see professor.entidades.Universidade#removerDocumentoDoMonteDoCurso(estudantes.entidades.Documento, professor.entidades.CodigoCurso)
+     * @see professor.entidades.Universidade#devolverDocumentoParaMonteDoCurso(estudantes.entidades.Documento, professor.entidades.CodigoCurso) 
+     */
+    public void trabalhar(){
+        
+    }
+}
---
+++ src/estudantes/entidades/Documento.java
@@ -0,0 +1,12 @@
+package estudantes.entidades;
+
+/**
+ * Classe que representa um documento genérico.
+ * <br><br>
+ * <strong>Seu trabalho começa aqui...</strong>
+ * 
+ * @author coloque os nomes dos autores aqui
+ */
+public abstract class Documento {
+    
+}
